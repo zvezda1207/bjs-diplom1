af69eb2e02812756cf2d2c9c74cc8a238d4705b2
Ira Tka4eva <ira.tka4eva2011@yandex.ru>
2024-10-13 16:29:14 +0300

    Страница регистрации работает

---
+++ public/loginPage.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const userForm = new UserForm();
+
+userForm.loginFormCallback = function(data) {
+    ApiConnector.login(data, (response) => {
+        console.log(response);
+        if (response.success) {
+            location.reload();
+        } else {
+            userForm.setLoginErrorMessage(response.error);
+        }
+    });
+};
+
+userForm.registerFormCallback = function(data) {
+    ApiConnector.register(data, (response) => {
+        console.log(response);
+        if (response.success) {
+            location.reload();
+        } else {
+            userForm.setRegisterErrorMessage(response.error);
+        }
+    });
+};
+
+
+
+
+
